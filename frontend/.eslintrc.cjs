73180faf6491e0fc44cce24aedc6d361c4ff2088
seoh7dev <seoh7dev@gmail.com>
2024-07-22 10:46:44 +0900

    ✨ feat : Front TypeScript 추가

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -10,9 +10,12 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
+    "plugin:@typescript-eslint/recommended",
   ],
 
   ignorePatterns: ["build", "dist", "public"],
+
+  parser: "@typescript-eslint/parser",
 
   parserOptions: {
     ecmaFeatures: {
@@ -22,19 +25,38 @@ module.exports = {
     sourceType: "module",
   },
 
-  plugins: ["react", "react-hooks", "prettier"],
+  plugins: ["react", "react-hooks", "prettier", "@typescript-eslint"],
 
   rules: {
     "prettier/prettier": ["error"],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
   },
 
   settings: {
     react: {
       version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
     },
   },
 };
