22f47a91540756992a5c9625454b143a28d31e33
seojiheun <wlgmsaudqls@naver.com>
2024-07-23 10:48:53 +0900

    :sparkles: Feat : Member swagger 작성

--- backend/src/main/java/com/ssafy/hifive/domain/board/dto/response/BoardResponseDto.java
+++ backend/src/main/java/com/ssafy/hifive/domain/board/dto/response/BoardResponseDto.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public final class BoardResponseDto {
-	private Long boardId;
+	private long boardId;
 	private String boardImg;
 	private LocalDateTime createdDate;
 	private String contents;
--- backend/src/main/java/com/ssafy/hifive/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/hifive/domain/member/service/MemberService.java
@@ -1,4 +1,19 @@
 package com.ssafy.hifive.domain.member.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.hifive.domain.member.dto.request.MemberNicknameDto;
+import com.ssafy.hifive.domain.member.dto.response.MemberResponseDto;
+import com.ssafy.hifive.domain.member.entity.Member;
+
+@Service
 public class MemberService {
+	public ResponseEntity<MemberResponseDto> getMemberDetail(Member member) {
+		return ResponseEntity.ok(null);
+	}
+
+	public void nicknameCheck(MemberNicknameDto memberNicknameDto, Member member) {
+		return;
+	}
 }
