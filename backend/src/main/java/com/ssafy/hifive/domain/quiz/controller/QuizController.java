c28a644e89513bf9af2a61f22ab999edfa6a6b24
seojiheun <wlgmsaudqls@naver.com>
2024-07-23 17:34:45 +0900

    :construction: quiz swagger 작성 중

--- backend/src/main/java/com/ssafy/hifive/domain/quiz/controller/QuizController.java
+++ backend/src/main/java/com/ssafy/hifive/domain/quiz/controller/QuizController.java
@@ -1,4 +1,14 @@
 package com.ssafy.hifive.domain.quiz.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/quiz")
+@RequiredArgsConstructor
+@Tag(name = "quiz", description = "퀴즈 관련 API")
 public class QuizController {
 }
