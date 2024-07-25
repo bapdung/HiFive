a10452d714430f4573c97671d07e7a2f8647c4c0
seoh7dev <seoh7dev@gmail.com>
2024-07-25 11:07:52 +0900

    💄 design : 마이페이지 '나의 활동' 글씨 크기 설정

--- frontend/src/pages/Mypage/MyPage.Reservation.tsx
+++ frontend/src/pages/Mypage/MyPage.Reservation.tsx
@@ -4,14 +4,14 @@ function Reservation() {
   return (
     <>
       <div className="mt-6">
-        <span className="text-primary-text pr-5 mr-5 border-r-2 border-gray-700">
+        <span className="text-primary-text pr-5 mr-5 border-r-2 border-gray-700 text-h4">
           예정 팬미팅
         </span>
-        <span>지난 팬미팅</span>
+        <span className="text-h4">지난 팬미팅</span>
       </div>
       <div className="flex w-full justify-end mr-28 mt-6">
-        <span className="mr-2.5 text-primary-text">최신순</span>
-        <span>과거순</span>
+        <span className="mr-2.5 text-primary-text text-medium">최신순</span>
+        <span className="text-medium">과거순</span>
       </div>
       <div className="mt-6 flex flex-wrap px-10 box-border justify-start gap-8">
         <FanmeetingInfo />
