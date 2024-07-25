9e2479a1bfdf87566ed0e69f4d99eab39d9deb07
kiminchae <alsco9707@gmail.com>
2024-07-25 14:55:16 +0900

    ✨ Feat : 티켓 상세페이지 퍼블리싱

--- frontend/src/pages/TicketPage/TicketPage.Detail.tsx
+++ frontend/src/pages/TicketPage/TicketPage.Detail.tsx
@@ -7,7 +7,7 @@ import Info from "./TicketPage.DetailInfo";
 
 function Detail() {
   return (
-    <div className="my-10 flex justify-center">
+    <div className="my-10 flex justify-center w-full">
       <div className="w-[60%] bg-white rounded-[25px] p-10">
         <div className="w-full flex">
           <img
@@ -46,6 +46,41 @@ function Detail() {
         </div>
         <TimeTable />
         <Info />
+      </div>
+      <div className="bg-white rounded-[25px] p-10 ml-8 max-w-[22%] h-fit sticky top-4">
+        <h2 className="text-h2 mb-12">
+          팬미팅제목팬미팅제목팬미팅제목팬미팅제목팬미팅제목팬미팅제목
+        </h2>
+        <div>
+          <p className="flex mb-2.5">
+            <span className="w-20 text-gray-700">날짜</span>
+            <span>2024년 7월 15일 월요일</span>
+          </p>
+          <p className="flex mb-2.5">
+            <span className="w-20 text-gray-700">시작시간</span>
+            <span>18:00</span>
+          </p>
+          <p className="flex mb-2.5">
+            <span className="w-20 text-gray-700">진행시간</span>
+            <span>120분</span>
+          </p>
+          <p className="flex mb-2.5">
+            <span className="w-20 text-gray-700">참가인원</span>
+            <span>30명</span>
+          </p>
+          <p className="flex mb-2.5">
+            <span className="w-20 text-gray-700">가격</span>
+            <span>50,000원</span>
+          </p>
+        </div>
+        <div className="mt-20">
+          <p className="text-gray-500 text-sm text-center mb-1">
+            잠깐! 예매 전 하단의 예매 안내 사항을 꼭 읽어주세요!
+          </p>
+          <button type="button" className="btn-lg w-full">
+            예매하기
+          </button>
+        </div>
       </div>
     </div>
   );
