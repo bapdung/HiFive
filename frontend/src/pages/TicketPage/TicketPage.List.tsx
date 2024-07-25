c7703abe5b7c8daf0a5e9c6f02da6ddd2d4afd2c
kiminchae <alsco9707@gmail.com>
2024-07-25 15:36:56 +0900

    ✨ Feat : 티켓 관련 모달 퍼블리싱

--- frontend/src/pages/TicketPage/TicketPage.List.tsx
+++ frontend/src/pages/TicketPage/TicketPage.List.tsx
@@ -1,10 +1,14 @@
 import SuccessModal from "./TicketPage.SuccessModal";
+import FailureModal from "./TicketPage.FailureModal";
+import WaitingModal from "./TicketPage.WaitingModal";
 
 function List() {
   return (
     <div className="my-10">
       <h1>티켓리스트</h1>
       <SuccessModal />
+      <FailureModal />
+      <WaitingModal />
     </div>
   );
 }
