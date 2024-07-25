05867802b5e9ed82871f34d79e208942b04ca2e5
kiminchae <alsco9707@gmail.com>
2024-07-25 15:25:00 +0900

    ✨ Feat : 예매성공 모달 퍼블리싱

--- frontend/src/pages/TicketPage/TicketPage.List.tsx
+++ frontend/src/pages/TicketPage/TicketPage.List.tsx
@@ -1,7 +1,10 @@
+import SuccessModal from "./TicketPage.SuccessModal";
+
 function List() {
   return (
     <div className="my-10">
       <h1>티켓리스트</h1>
+      <SuccessModal />
     </div>
   );
 }
---
+++ frontend/src/pages/TicketPage/TicketPage.SuccessModal.tsx
@@ -0,0 +1,20 @@
+function SuccessModal() {
+  return (
+    <div className="w-[50rem] bg-white flex flex-col items-center h-[26rem] justify-center">
+      <h1 className="text-primary-text text-h2 my-4">예매 완료되었습니다!</h1>
+      <h2 className="text-small mb-4">
+        팬미팅 입장 전에 신분증 등록을 진행해주세요!
+      </h2>
+      <div>
+        <button type="button" className="btn-outline-md">
+          예매 확인하기
+        </button>
+        <button type="button" className="btn-light-md ml-6">
+          신분증 등록하기
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SuccessModal;
