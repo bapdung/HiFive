2d3c968063b8a5e3fed6ba418e0ff5151e6dd5d3
seoh7dev <seoh7dev@gmail.com>
2024-07-29 15:33:06 +0900

    💄 design : 마이페이지 sidebar height 채우도록 수정

--- frontend/src/pages/Mypage/MyPage.Sidebar.tsx
+++ frontend/src/pages/Mypage/MyPage.Sidebar.tsx
@@ -16,47 +16,49 @@ function Sidebar() {
   };
 
   return (
-    <div className="w-64 h-full bg-white pt-10 absolute left-0 shadow-side-shadow p-2.5">
-      <Link
-        to="/mypage/reservation"
-        className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/reservation" || activeLink === "/mypage/reservation" ? "bg-gray-200" : ""}`}
-        onClick={(e) => sidebarClick(e, "/mypage/reservation")}
-      >
-        <img src={personIcon} alt="사람아이콘" className="w-4 h-4 mr-3.5" />
-        <span className="text-h5">나의 활동</span>
-      </Link>
-      <Link
-        to="/mypage/my-info"
-        className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/my-info" || activeLink === "/mypage/my-info" ? "bg-gray-200" : ""}`}
-        onClick={(e) => sidebarClick(e, "/mypage/my-info")}
-      >
-        <img src={pencilIcon} alt="연필아이콘" className="w-4 h-4 mr-3.5" />
-        <span className="text-h5">개인 정보 수정</span>
-      </Link>
-      <Link
-        to="/mypage/gallery"
-        className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/gallery" || activeLink === "/mypage/gallery" ? "bg-gray-200" : ""}`}
-        onClick={(e) => sidebarClick(e, "/mypage/gallery")}
-      >
-        <img src={galleryIcon} alt="사진아이콘" className="w-4 h-4 mr-3.5" />
-        <span className="text-h5">추억 갤러리</span>
-      </Link>
-      <Link
-        to="/mypage/idcard"
-        className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/idcard" || activeLink === "/mypage/idcard" ? "bg-gray-200" : ""}`}
-        onClick={(e) => sidebarClick(e, "/mypage/idcard")}
-      >
-        <img src={faceIcon} alt="얼굴아이콘" className="w-4 h-4 mr-3.5" />
-        <span className="text-h5">신분증 등록</span>
-      </Link>
-      <Link
-        to="/mypage/point"
-        className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/point" || activeLink === "/mypage/point" ? "bg-gray-200" : ""}`}
-        onClick={(e) => sidebarClick(e, "/mypage/point")}
-      >
-        <img src={pointIcon} alt="포인트아이콘" className="w-4 하-4 mr-3.5" />
-        <span className="text-h5">충전 및 내역 조회</span>
-      </Link>
+    <div className="overflow-hidden h-full">
+      <div className="mb-[-5000px] pb-[1000px] w-64 h-full bg-white pt-10 absolute left-0 shadow-side-shadow p-2.5">
+        <Link
+          to="/mypage/reservation"
+          className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/reservation" || activeLink === "/mypage/reservation" ? "bg-gray-200" : ""}`}
+          onClick={(e) => sidebarClick(e, "/mypage/reservation")}
+        >
+          <img src={personIcon} alt="사람아이콘" className="w-4 h-4 mr-3.5" />
+          <span className="text-h5">나의 활동</span>
+        </Link>
+        <Link
+          to="/mypage/my-info"
+          className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/my-info" || activeLink === "/mypage/my-info" ? "bg-gray-200" : ""}`}
+          onClick={(e) => sidebarClick(e, "/mypage/my-info")}
+        >
+          <img src={pencilIcon} alt="연필아이콘" className="w-4 h-4 mr-3.5" />
+          <span className="text-h5">개인 정보 수정</span>
+        </Link>
+        <Link
+          to="/mypage/gallery"
+          className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/gallery" || activeLink === "/mypage/gallery" ? "bg-gray-200" : ""}`}
+          onClick={(e) => sidebarClick(e, "/mypage/gallery")}
+        >
+          <img src={galleryIcon} alt="사진아이콘" className="w-4 h-4 mr-3.5" />
+          <span className="text-h5">추억 갤러리</span>
+        </Link>
+        <Link
+          to="/mypage/idcard"
+          className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/idcard" || activeLink === "/mypage/idcard" ? "bg-gray-200" : ""}`}
+          onClick={(e) => sidebarClick(e, "/mypage/idcard")}
+        >
+          <img src={faceIcon} alt="얼굴아이콘" className="w-4 h-4 mr-3.5" />
+          <span className="text-h5">신분증 등록</span>
+        </Link>
+        <Link
+          to="/mypage/point"
+          className={`flex items-center h-20 pl-10 rounded-2xl ${location.pathname === "/mypage/point" || activeLink === "/mypage/point" ? "bg-gray-200" : ""}`}
+          onClick={(e) => sidebarClick(e, "/mypage/point")}
+        >
+          <img src={pointIcon} alt="포인트아이콘" className="w-4 하-4 mr-3.5" />
+          <span className="text-h5">충전 및 내역 조회</span>
+        </Link>
+      </div>
     </div>
   );
 }
