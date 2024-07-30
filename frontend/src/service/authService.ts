9ee7f50c53c376774cf72871242ee8b4c2940ce5
seoh7dev <seoh7dev@gmail.com>
2024-07-30 15:37:22 +0900

    ✨ feat : 카카오 로그인 백엔드와 연결 테스트

--- frontend/src/service/authService.ts
+++ frontend/src/service/authService.ts
@@ -1,10 +1,27 @@
-const kakaoLogin = () => {
+import axios from "axios";
+
+export const kakaoLogin = () => {
   const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
-  const REDIRECT_URI = "http://localhost:3000/kakao/auth";
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+  const REDIRECT_URI = "http://i11a107.p.ssafy.io:8080/login/oauth2/code/kakao";
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   window.location.href = kakaoURL;
-  const token = new URL(window.location.href).searchParams.get("code");
 };
 
-export default kakaoLogin;
+export const handleKakaoCallback = async () => {
+  const code = new URL(window.location.href).searchParams.get("code");
+
+  if (code) {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_END_POINT}/auth/callback`,
+        {
+          code,
+        },
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
