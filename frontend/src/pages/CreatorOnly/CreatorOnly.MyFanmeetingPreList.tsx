37648b0ab88a4c65f507ffd1e2e9bc2463424f20
김민채 <alsco9707@gmail.com>
2024-07-31 21:30:33 +0900

    ✨ Feat : 예정된 팬미팅 axios 및 props 관리

--- frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingPreList.tsx
+++ frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingPreList.tsx
@@ -1,14 +1,52 @@
-// import MyFanmeetingItem from "./CreatorOnly.MyFanmeetingItem";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+import MyFanmeetingItem from "./CreatorOnly.MyFanmeetingItem";
+import client from "../../client";
+import useOnMounted from "../../utils/useOnMounted";
+
+interface Fanmeeting {
+  title: string;
+  posterImg: string;
+  startDate: string;
+}
 
 function MyFanmeetingPreList() {
+  const location = useLocation();
+  const creatorId = parseInt(location.pathname.split("/")[3], 10);
+  const [fanmeetings, setFanmeetings] = useState<Fanmeeting[]>([]);
+  const fetchFanmeetings = async () => {
+    const token = process.env.REACT_APP_AUTHORIZATION as string;
+    try {
+      if (!token) {
+        return;
+      }
+      const response = await client(token).get(
+        `/api/fanmeeting/completed/creator/${creatorId}`,
+      );
+      setFanmeetings(response.data);
+      console.log(fanmeetings);
+    } catch (error) {
+      console.error("Error sending post request:", error);
+    }
+  };
+  useOnMounted(() => fetchFanmeetings);
   return (
     <div className="w-full flex flex-wrap">
-      <p>아직 미완료</p>
-      {/* <MyFanmeetingItem isDone={false} />
-      <MyFanmeetingItem isDone={false} />
-      <MyFanmeetingItem isDone={false} />
-      <MyFanmeetingItem isDone={false} />
-      <MyFanmeetingItem isDone={false} /> */}
+      {fanmeetings.length === 0 ? (
+        <p className="text-xl text-gray-500 mr-auto ml-auto my-5">
+          생성한 팬미팅이 없습니다.
+        </p>
+      ) : null}
+      {fanmeetings.map((fanmeeting) => (
+        <MyFanmeetingItem
+          key={fanmeeting.title}
+          title={fanmeeting.title}
+          posterImg={fanmeeting.posterImg}
+          startDate={fanmeeting.startDate}
+          isDone={false}
+        />
+      ))}
     </div>
   );
 }
