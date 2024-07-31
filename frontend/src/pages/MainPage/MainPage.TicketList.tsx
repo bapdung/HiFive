529d042cda32497f58ac17186c92aee607c192d9
Wonwoo Cho <ww8637@naver.com>
2024-07-31 17:19:00 +0900

    ✨ Feat : 메인 페이지 티켓 캐러셀 퍼블리싱

--- frontend/src/pages/MainPage/MainPage.TicketList.tsx
+++ frontend/src/pages/MainPage/MainPage.TicketList.tsx
@@ -1,7 +1,10 @@
+import TicketCarousel from "./MainPage.TicketCarousel";
+
 function TicketList() {
   return (
-    <div className="w-[1900px] flex flex-col h-[755px] bg-gray-700 rounded-3xl py-8 items-center">
-      <span className="text-h3 text-[#FFFFFF]">이름님이 예매한 티켓</span>
+    <div className="w-[1900px] flex flex-col h-[755px] bg-gray-700 rounded-3xl py-8 items-center overflow-hidden">
+      <span className="text-h4 text-[#FFFFFF] mb-8">이름님이 예매한 티켓</span>
+      <TicketCarousel />
     </div>
   );
 }
