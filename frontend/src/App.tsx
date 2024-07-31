cf5997eed16c94c45fb960c1698fd51d6792d8b7
seoh7dev <seoh7dev@gmail.com>
2024-07-31 15:51:33 +0900

    ✨ feat : 크리에이터 입점 신청 페이지 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import TicketDetail from "./pages/TicketPage/TicketPage.Detail";
 import Question from "./pages/TicketPage/TicketPage.Question";
 import StoryForm from "./pages/TicketPage/TicketPage.StoryForm";
 import CreatorList from "./pages/CreatorListPage/CreatorListPage";
+import JoinCreator from "./pages/JoinCreatorPage/JoinCreatorPage";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
             element={<StoryForm />}
           />
           <Route path="/creator/list" element={<CreatorList />} />
+          <Route path="/parther" element={<JoinCreator />} />
         </Routes>
       </main>
       <Footer />
