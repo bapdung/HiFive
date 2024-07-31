58802f38a5de14aa93403ad1aecda3f9660647f2
seoh7dev <seoh7dev@gmail.com>
2024-07-31 14:18:04 +0900

    🐛 fix : 카카오 로그인 백엔드와 연결 방식 수정

--- frontend/src/service/authService.ts
+++ frontend/src/service/authService.ts
@@ -1,27 +1,6 @@
-import axios from "axios";
-
-export const kakaoLogin = () => {
-  const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
-  const REDIRECT_URI = "http://i11a107.p.ssafy.io:8080/login/oauth2/code/kakao";
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
-  window.location.href = kakaoURL;
+const kakaoLogin = () => {
+  const REDIRECT_URI = `${process.env.REACT_APP_END_POINT}/oauth2/authorization/kakao`;
+  window.location.href = REDIRECT_URI;
 };
 
-export const handleKakaoCallback = async () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-
-  if (code) {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_END_POINT}/auth/callback`,
-        {
-          code,
-        },
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
+export default kakaoLogin;
