4cca305692b0a87f737a327b31b57ed7211d2a1e
seoh7dev <seoh7dev@gmail.com>
2024-08-01 11:14:57 +0900

    ✨ feat : getRefreshToken, getAccessToken 함수 구현

--- frontend/src/service/authService.ts
+++ frontend/src/service/authService.ts
@@ -1,6 +1,20 @@
-const kakaoLogin = () => {
+import axios from "axios";
+
+export const kakaoLogin = () => {
   const REDIRECT_URI = `${process.env.REACT_APP_END_POINT}/oauth2/authorization/kakao`;
   window.location.href = REDIRECT_URI;
 };
 
-export default kakaoLogin;
+export const getRefreshToken = () => {
+  const refreshToken = document.cookie;
+  return refreshToken;
+};
+
+export const getAccessToken = async (refreshToken: string) => {
+  const response = await axios.post(
+    `${process.env.REACT_APP_END_POINT}/api/auth/refresh`,
+    refreshToken,
+  );
+
+  return response.data.accessToken;
+};
