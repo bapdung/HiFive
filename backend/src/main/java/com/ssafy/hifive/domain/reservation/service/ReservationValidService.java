298ab1584ab1dacdcd642b937654858e582e00e3
seojiheun <wlgmsaudqls@naver.com>
2024-08-01 09:10:07 +0900

    :recycle: Refactor : Reservation valid 로직 분리

--- backend/src/main/java/com/ssafy/hifive/domain/reservation/service/ReservationValidService.java
+++ backend/src/main/java/com/ssafy/hifive/domain/reservation/service/ReservationValidService.java
@@ -2,6 +2,7 @@ package com.ssafy.hifive.domain.reservation.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.hifive.domain.member.entity.Member;
 import com.ssafy.hifive.global.error.ErrorCode;
 import com.ssafy.hifive.global.error.type.BadRequestException;
 
@@ -10,6 +11,12 @@ public class ReservationValidService {
 	public void ReservationIsValid(int remainingTicket) {
 		if(remainingTicket <= 0) {
 			throw new BadRequestException(ErrorCode.TICKET_SOLD_OUT);
+		}
+	}
+
+	public void PointIsValid(Member member, int price) {
+		if(price > member.getPoint()){
+			throw new BadRequestException(ErrorCode.WANT_FOR_MONEY);
 		}
 	}
 }
