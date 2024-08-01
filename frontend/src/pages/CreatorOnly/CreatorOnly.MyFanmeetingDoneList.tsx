8870ada18d0ad85cdc96095617c057e890b49b5c
kiminchae <alsco9707@gmail.com>
2024-08-01 13:31:58 +0900

    ♻️ Refactor : 날짜 포맷 함수 모듈화

--- frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingDoneList.tsx
+++ frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingDoneList.tsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 import MyFanmeetingItem from "./CreatorOnly.MyFanmeetingItem";
 import client from "../../client";
+import formatDate from "../../utils/formatDate";
 
 interface MyFanmeetingDoneListProps {
   isRecent: boolean;
@@ -18,7 +19,6 @@ interface Fanmeeting {
 const MyFanmeetingDoneList: React.FC<MyFanmeetingDoneListProps> = ({
   isRecent,
 }) => {
-  // const [lastFanmeeting, setLastFanmeeting] = useState<number | null>();
   const params = {
     sort: isRecent ? "desc" : "asc",
   };
@@ -36,9 +36,6 @@ const MyFanmeetingDoneList: React.FC<MyFanmeetingDoneListProps> = ({
         { params },
       );
       setFanmeetings(response.data);
-      if (response.data.length > 0) {
-        // setLastFanmeeting(response.data[response.data.length - 1].fanmeetingId);
-      }
     } catch (error) {
       console.error("Error sending post request:", error);
     }
@@ -47,19 +44,6 @@ const MyFanmeetingDoneList: React.FC<MyFanmeetingDoneListProps> = ({
   useEffect(() => {
     fetchFanmeetings();
   }, [creatorId, isRecent]);
-
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
-    const day = String(date.getDate()).padStart(2, "0");
-
-    const weekDayNames = ["일", "월", "화", "수", "목", "금", "토"];
-    const weekDay = weekDayNames[date.getDay()];
-
-    return `${year}년 ${month}월 ${day}일 (${weekDay})`;
-  };
 
   return (
     <div className="w-full flex flex-wrap">
