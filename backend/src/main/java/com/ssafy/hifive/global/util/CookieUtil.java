20d9ebb4fa145b2e474d03996755902533ec87a5
rkdalstj4505 <rkdalstj4505@naver.com>
2024-08-02 10:22:17 +0900

    :ambulance: Hotfix : cors 수정

--- backend/src/main/java/com/ssafy/hifive/global/util/CookieUtil.java
+++ backend/src/main/java/com/ssafy/hifive/global/util/CookieUtil.java
@@ -18,12 +18,9 @@ public class CookieUtil {
 		cookie.setPath("/");
 		cookie.setMaxAge(maxAge);
 		cookie.setSecure(secure);
+		cookie.setAttribute("SameSite", "None");
+		response.addCookie(cookie);
 
-		String sameSite = "None";
-		response.addHeader("Set-Cookie",
-			String.format("%s=%s; Max-Age=%d; Path=%s; Secure=%s; HttpOnly=%s; SameSite=%s",
-				cookie.getName(), cookie.getValue(), cookie.getMaxAge(), cookie.getPath(),
-				cookie.getSecure() ? "Secure" : "", cookie.isHttpOnly() ? "HttpOnly" : "", sameSite));
 	}
 
 	public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
