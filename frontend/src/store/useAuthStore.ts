3cf4a8beec28dfa2ddfd0151db0a1f12c6e5fc29
rkdalstj4505 <rkdalstj4505@naver.com>
2024-08-04 02:31:50 +0900

    :sparkles: Feat : token 검증

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -4,27 +4,60 @@ import { getAccessToken } from "../service/authService";
 interface AuthState {
   accessToken: string | null;
   setAccessToken: (token: string | null) => void;
-  fetchTokens: () => Promise<void>;
+  validateAndGetToken: () => Promise<string | null>;
 }
 
 const useAuthStore = create<AuthState>((set) => ({
-  accessToken: null,
+  accessToken: localStorage.getItem("accessToken"),
 
   setAccessToken: (token) => {
     set({ accessToken: token });
+    if (token) {
+      localStorage.setItem("accessToken", token);
+    } else {
+      localStorage.removeItem("accessToken");
+    }
   },
 
-  fetchTokens: async () => {
-    try {
-      const newAccessToken = await getAccessToken();
-      if (newAccessToken) {
-        localStorage.setItem("accessToken", newAccessToken);
-        set({ accessToken: newAccessToken });
-        window.location.reload();
+  validateAndGetToken: async () => {
+    let accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      const base64Url = accessToken.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split("")
+          .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+          .join(""),
+      );
+      const decodedToken = JSON.parse(jsonPayload);
+      const tokenExpiration = decodedToken.exp;
+      const now = Math.floor(Date.now() / 1000);
+
+      if (tokenExpiration < now) {
+        accessToken = await getAccessToken();
+        if (accessToken) {
+          localStorage.setItem("accessToken", accessToken);
+          set({ accessToken });
+          window.location.reload(); // 새로고침 추가
+        } else {
+          console.error("Failed to refresh access token");
+          return null;
+        }
       }
-    } catch (error) {
-      console.error("Failed to fetch tokens:", error);
+    } else {
+      accessToken = await getAccessToken();
+      if (accessToken) {
+        localStorage.setItem("accessToken", accessToken);
+        set({ accessToken });
+        window.location.reload(); // 새로고침 추가
+      } else {
+        console.error("Failed to get access token");
+        return null;
+      }
     }
+
+    return accessToken;
   },
 }));
 
