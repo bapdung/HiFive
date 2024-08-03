3718fa91e2cd3f0af8b7e08baef36cc03ec2e873
seoh7dev <seoh7dev@gmail.com>
2024-08-03 16:38:08 +0900

    ✨ Feat : 마이페이지 '나의 활동' API 연결 후 화면 표시

--- frontend/src/components/FanmeetingInfo.tsx
+++ frontend/src/components/FanmeetingInfo.tsx
@@ -1,10 +1,28 @@
-function FanmeetingInfo() {
+type Fanmeeting = {
+  fanmeetingId: number;
+  title: string;
+  posterImg: string;
+  creatorName: string;
+  startTime: string;
+};
+
+interface FanmeetingInfoProps {
+  fanmeeting: Fanmeeting;
+}
+
+function FanmeetingInfo({ fanmeeting }: FanmeetingInfoProps) {
+  const { title, posterImg, startTime } = fanmeeting;
+
   return (
     <div className="w-56 mb-5">
-      <div className="w-full h-72 bg-gray-300 rounded-xl" />
+      <img
+        src={posterImg}
+        alt="팬미팅포스터"
+        className="w-full h-72 bg-gray-300 rounded-xl"
+      />
       <div className="flex flex-col items-center mt-2.5">
-        <h4 className="text-h5">어쩌면 해피엔딩</h4>
-        <span className="text-h6 text-gray-600">2024. 07. 23</span>
+        <h4 className="text-h5">{title}</h4>
+        <span className="text-h6 text-gray-600">{startTime}</span>
       </div>
     </div>
   );
