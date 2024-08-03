eef97125a3fe1107d580f2fee27cdeec94ff997b
seoh7dev <seoh7dev@gmail.com>
2024-08-03 16:38:08 +0900

    ✨ Feat : 마이페이지 '나의 활동' API 연결 후 화면 표시

--- frontend/src/pages/Mypage/MyPage.Reservation.tsx
+++ frontend/src/pages/Mypage/MyPage.Reservation.tsx
@@ -1,46 +1,54 @@
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, useEffect } from "react";
 import useAuthStore from "../../store/useAuthStore";
 import client from "../../client";
 
 import FanmeetingInfo from "../../components/FanmeetingInfo";
 
-type FanmeetingInfo = {
+type Fanmeeting = {
   fanmeetingId: number;
   title: string;
+  posterImg: string;
   creatorName: string;
   startTime: string;
 };
 
-type FanmeetingList = FanmeetingInfo[];
+type FanmeetingList = Fanmeeting[];
 
 function Reservation() {
   const token = useAuthStore((state) => state.accessToken);
 
-  const [fanmeeting, setFanmeeting] = useState<FanmeetingList>([]);
+  const [fanmeetingList, setFanmeetingList] = useState<FanmeetingList>([]);
   const [status, setStatus] = useState("scheduled");
   const [sort, setSort] = useState("desc");
   const [top, setTop] = useState(0);
 
-  const getFanmeeting = async () => {
-    const params: {
-      sort: string;
-      top?: number;
-    } = { sort };
+  useEffect(() => {
+    const getFanmeeting = async () => {
+      const params: {
+        sort: string;
+        top?: number;
+      } = { sort };
 
-    if (token) {
-      const response: FanmeetingList = await client(token).get(
-        `api/fanmeeting/${status}/fan`,
-        { params },
-      );
-      setFanmeeting(response);
-
-      if (fanmeeting) {
-        setTop(fanmeeting.length);
+      if (top) {
+        params.top = top;
       }
-      params.top = top;
-      console.log(fanmeeting);
-    }
-  };
+
+      if (token) {
+        const response = await client(token).get(
+          `api/fanmeeting/${status}/fan`,
+          {
+            params,
+          },
+        );
+
+        setFanmeetingList([...fanmeetingList, ...response.data]);
+        setTop(top + response.data.length);
+      }
+    };
+
+    getFanmeeting();
+    // eslint-disable-next-line
+  }, [token, status, sort]);
 
   const changeStatus = (e: MouseEvent<HTMLSpanElement>) => {
     const content = e.currentTarget.textContent;
@@ -50,8 +58,8 @@ function Reservation() {
     } else if (content === "지난 팬미팅") {
       setStatus("completed");
     }
-
-    getFanmeeting();
+    setTop(0);
+    setFanmeetingList([]);
   };
 
   const changeSort = (e: MouseEvent<HTMLSpanElement>) => {
@@ -62,22 +70,22 @@ function Reservation() {
     } else if (content === "과거순") {
       setSort("asc");
     }
-
-    getFanmeeting();
+    setTop(0);
+    setFanmeetingList([]);
   };
 
   return (
     <>
       <div className="mt-6">
         <span
-          className="text-primary-text pr-5 mr-5 border-r-2 border-gray-200 text-h4"
+          className={`pr-5 mr-5 border-r-2 border-gray-200 text-h4 ${status === "scheduled" ? "text-primary-text" : ""}`}
           onClick={(e) => changeStatus(e)}
           role="presentation"
         >
           예정 팬미팅
         </span>
         <span
-          className="text-h4"
+          className={`text-h4 ${status === "completed" ? "text-primary-text" : ""}`}
           onClick={(e) => changeStatus(e)}
           role="presentation"
         >
@@ -86,14 +94,14 @@ function Reservation() {
       </div>
       <div className="flex w-full justify-end mr-28 mt-6">
         <span
-          className="mr-2.5 text-primary-text text-medium"
+          className={`mr-2.5 text-medium ${sort === "desc" ? "text-primary-text" : ""}`}
           onClick={(e) => changeSort(e)}
           role="presentation"
         >
           최신순
         </span>
         <span
-          className="text-medium"
+          className={`text-medium ${sort === "asc" ? "text-primary-text" : ""}`}
           onClick={(e) => changeSort(e)}
           role="presentation"
         >
@@ -101,13 +109,12 @@ function Reservation() {
         </span>
       </div>
       <div className="mt-6 flex flex-wrap px-10 box-border justify-start gap-9">
-        <FanmeetingInfo />
-        <FanmeetingInfo />
-        <FanmeetingInfo />
-        <FanmeetingInfo />
-        <FanmeetingInfo />
-        <FanmeetingInfo />
-        <FanmeetingInfo />
+        {fanmeetingList.map((fanmeeting) => (
+          <FanmeetingInfo
+            fanmeeting={fanmeeting}
+            key={fanmeeting.fanmeetingId}
+          />
+        ))}
       </div>
     </>
   );
