0e570d18e174187a50afe0e0608bf805b1996806
kiminchae <alsco9707@gmail.com>
2024-08-03 13:45:10 +0900

    🐛 Fix : 백엔드 파일 실수로 커밋한거 되돌리기

--- backend/src/main/java/com/ssafy/hifive/global/config/WebOAuthSecurityConfig.java
+++ backend/src/main/java/com/ssafy/hifive/global/config/WebOAuthSecurityConfig.java
@@ -116,7 +116,6 @@ public class WebOAuthSecurityConfig implements WebMvcConfigurer {
 		CorsConfiguration configuration = new CorsConfiguration();
 
 		configuration.setAllowedOrigins(Arrays.asList(
-			"http://localhost:3000",
 			"http://i11a107.p.ssafy.io",
 			"https://i11a107.p.ssafy.io",
 			"http://i11a107.p.ssafy.io:8080",
