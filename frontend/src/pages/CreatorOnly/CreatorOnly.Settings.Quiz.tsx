e90c291fb7571009974379a46a80951049bcd86e
kiminchae <alsco9707@gmail.com>
2024-08-04 21:57:06 +0900

    ✨ Feat : 질문 수정 구현

--- frontend/src/pages/CreatorOnly/CreatorOnly.Settings.Quiz.tsx
+++ frontend/src/pages/CreatorOnly/CreatorOnly.Settings.Quiz.tsx
@@ -22,21 +22,59 @@ const Quiz: React.FC<QuizProps> = ({ handleQuizOpen, handleQuizSequence }) => {
   const token = useAuthStore((state) => state.accessToken);
   const [allQuizzes, setAllQuizzes] = useState<Quiz[]>([]);
   const fanmeetingId = parseInt(location.pathname.split("/")[2], 10);
-  // console.log(fanmeetingId);
-  useEffect(() => {
-    const fetchAllQuizzes = async () => {
-      if (!token) {
-        return;
-      }
-      const response = await client(token).get(`api/quiz/${fanmeetingId}`);
-      // console.log(response.data);
-      setAllQuizzes(response.data);
-      handleQuizSequence(allQuizzes.length - 1);
-    };
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentQuiz, setCurrentQuiz] = useState<Quiz | null>(null);
+  const [currentQuizTitle, setCurrentQuizTitle] = useState("");
+  const [currentQuizDetail, setCurrentQuizDetail] = useState("");
+  const [currentQuizAnswer, setCurrentQuizAnswer] = useState<boolean | null>(
+    null,
+  );
 
+  const fetchAllQuizzes = async () => {
+    if (!token) {
+      return;
+    }
+    const response = await client(token).get(`api/quiz/${fanmeetingId}`);
+    // console.log(response.data);
+    setAllQuizzes(response.data);
+    handleQuizSequence(allQuizzes.length - 1);
+  };
+
+  useEffect(() => {
     fetchAllQuizzes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token, fanmeetingId, allQuizzes.length]);
+
+  const openInput = (quiz: Quiz) => {
+    setCurrentQuiz(quiz);
+    setIsEditing(true);
+    setCurrentQuizAnswer(quiz.answer);
+    setCurrentQuizTitle(quiz.problem);
+    setCurrentQuizDetail(quiz.detail);
+  };
+
+  const handleInputTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentQuizTitle(event.target.value);
+  };
+
+  const handleInputDetail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentQuizDetail(event.target.value);
+  };
+
+  const updateQuiz = async (buttonOfQuiz: Quiz) => {
+    if (!token || !currentQuiz || currentQuiz !== buttonOfQuiz) {
+      return;
+    }
+    const payload = {
+      problem: currentQuizTitle,
+      detail: currentQuizDetail,
+      sequence: buttonOfQuiz.sequence,
+      answer: currentQuizAnswer,
+    };
+    await client(token).put(`/api/quiz/${currentQuiz.id}`, payload);
+    setIsEditing(false);
+    fetchAllQuizzes();
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -53,18 +91,86 @@ const Quiz: React.FC<QuizProps> = ({ handleQuizOpen, handleQuizSequence }) => {
             key={quiz.id}
             className="border-2 border-secondary-700 rounded-[20px] w-[30%] flex flex-col items-center min-h-48 py-[1rem] px-8 justify-between bg-white"
           >
-            <p className="text-h5 flex justify-between w-full">
-              <span>문제 {index + 1}</span>{" "}
-              <span
-                className={quiz.answer ? "text-secondary" : "text-primary-text"}
+            {!isEditing || currentQuiz !== quiz ? (
+              <button
+                type="button"
+                className="text-h5 flex justify-between w-full"
+                onClick={() => openInput(quiz)}
               >
-                정답 {quiz.answer ? "O" : "X"}
-              </span>
-            </p>
-            <p className="text-large text-gray-700">{quiz.problem}</p>
-            <p className="text-large text-gray-500">{quiz.detail}</p>
+                <span>문제 {index + 1}</span>{" "}
+                <span
+                  className={
+                    quiz.answer ? "text-secondary" : "text-primary-text"
+                  }
+                >
+                  정답 {quiz.answer ? "O" : "X"}
+                </span>
+              </button>
+            ) : null}
+            {isEditing && currentQuiz === quiz ? (
+              <div>
+                <button
+                  type="button"
+                  className={
+                    currentQuizAnswer
+                      ? "creator-btn-md"
+                      : "creator-btn-light-md"
+                  }
+                  onClick={() => setCurrentQuizAnswer(true)}
+                >
+                  O
+                </button>
+                <button
+                  type="button"
+                  className={
+                    currentQuizAnswer === false
+                      ? "btn-md ml-2"
+                      : "btn-light-md ml-2"
+                  }
+                  onClick={() => setCurrentQuizAnswer(false)}
+                >
+                  X
+                </button>
+              </div>
+            ) : null}
+            {currentQuiz !== quiz || !isEditing ? (
+              <button
+                type="button"
+                className="text-large w-full"
+                onClick={() => openInput(quiz)}
+              >
+                <span className="text-gray-700 w-full block mb-2.5">
+                  {quiz.problem}
+                </span>
+                <span className="text-gray-500 w-full block mb-2.5">
+                  {quiz.detail}
+                </span>
+              </button>
+            ) : null}
+
+            {currentQuiz === quiz && isEditing ? (
+              <div>
+                <input
+                  type="text"
+                  className="focus:outline-none text-large text-gray-700 text-center mb-2.5 w-full"
+                  value={currentQuizTitle}
+                  onChange={handleInputTitle}
+                />
+                <input
+                  type="text"
+                  className="focus:outline-none text-large text-gray-500 text-center mb-2.5 w-full"
+                  value={currentQuizDetail}
+                  onChange={handleInputDetail}
+                />
+              </div>
+            ) : null}
+
             <div>
-              <button type="button" className="creator-btn-light-md">
+              <button
+                type="button"
+                className="creator-btn-light-md"
+                onClick={() => updateQuiz(quiz)}
+              >
                 수정하기
               </button>
               <button type="button" className="btn-light-md ml-2">
