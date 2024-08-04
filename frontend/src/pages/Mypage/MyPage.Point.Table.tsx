63f863941b7c1e0ef70fd811e9805a576a034a0e
seoh7dev <seoh7dev@gmail.com>
2024-08-04 23:16:21 +0900

    ✨ Feat : 마이페이지 포인트 충전 및 조회 기능 구현

--- frontend/src/pages/Mypage/MyPage.Point.Table.tsx
+++ frontend/src/pages/Mypage/MyPage.Point.Table.tsx
@@ -1,49 +1,102 @@
-// import { useEffect, useState } from "react";
-// import client from "../../client";
-// import useAuthStore from "../../store/useAuthStore";
+import { useEffect, useState } from "react";
+import client from "../../client";
+import useAuthStore from "../../store/useAuthStore";
 
 import preIcon from "../../assets/icons/preIcon.svg";
 import nextIcon from "../../assets/icons/nextIcon.svg";
 
-// type Point = {
-//   transactionDate: string;
-//   money: number;
-//   point: number;
-// };
+type PlusPoint = {
+  transactionDate: string;
+  money: number;
+  point: number;
+  totalPages: number;
+};
+
+type MinusPoint = {
+  transactionDate: string;
+  detail: number;
+  point: number;
+  totalPages: number;
+};
+
+type Params = {
+  period: number;
+  page: number;
+};
 
 interface TypeProps {
-  type: string;
+  type: "plus" | "minus";
 }
 
 function Table({ type }: TypeProps) {
-  // const token = useAuthStore((state) => state.accessToken);
+  const token = useAuthStore((state) => state.accessToken);
 
-  // const [plusPoint, setPlusPoint] = useState<Point[] | null>(null);
+  const [totalPage, setTotalPage] = useState<number>(1);
+  const [pointList, setPointList] = useState<PlusPoint[] | MinusPoint[] | null>(
+    null,
+  );
+  const [period, setPeriod] = useState<number>(3);
+  const [page, setPage] = useState<number>(1);
+  const [currentPageGroup, setCurrentPageGroup] = useState<number>(0);
 
-  // const getPlusPoint = async () => {
-  //   const params: {
-  //     perid: number;
-  //     page: number;
-  //   } = {
-  //     perid: 3,
-  //     page: 0,
-  //   };
+  const changePage = (newPage: number) => {
+    setPage(newPage);
+  };
 
-  //   if (token) {
-  //     const response = await client(token).get(`/api/point/plus`, {
-  //       params,
-  //     });
-  //     if (plusPoint) {
-  //       setPlusPoint([...plusPoint, ...response.data]);
-  //     } else {
-  //       setPlusPoint(response.data);
-  //     }
-  //   }
-  // };
+  const changePeriod = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPeriod(Number(e.target.value));
+  };
 
-  // useEffect(() => {
-  //   getPlusPoint();
-  // }, []);
+  useEffect(() => {
+    const getPoint = async () => {
+      const params: Params = {
+        period,
+        page: page - 1,
+      };
+
+      if (token) {
+        const response = await client(token).get(`/api/point/${type}`, {
+          params,
+        });
+
+        setPointList(response.data);
+        setTotalPage(response.data[0].totalPages);
+      }
+    };
+
+    getPoint();
+  }, [token, type, period, page]);
+
+  const handleNextPageGroup = () => {
+    if ((currentPageGroup + 1) * 5 < totalPage) {
+      setCurrentPageGroup(currentPageGroup + 1);
+    }
+  };
+
+  const handlePreviousPageGroup = () => {
+    if (currentPageGroup > 0) {
+      setCurrentPageGroup(currentPageGroup - 1);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    const startPage = currentPageGroup * 5 + 1;
+    const endPage = Math.min(startPage + 4, totalPage);
+    const pages = [];
+    for (let i = startPage; i <= endPage; i += 1) {
+      pages.push(
+        <div
+          key={i}
+          onClick={() => changePage(i)}
+          role="presentation"
+          className={page === i ? "text-primary-700" : ""}
+        >
+          {i}
+        </div>,
+      );
+    }
+    return pages;
+  };
 
   return (
     <>
@@ -51,7 +104,11 @@ function Table({ type }: TypeProps) {
         <h5 className="text-h5">
           {type === "plus" ? "충전 내역 조회" : "사용 내역 조회"}
         </h5>
-        <select name="period" className="text-small">
+        <select
+          name="period"
+          className="text-small"
+          onChange={(e) => changePeriod(e)}
+        >
           <option value="3">최근 3개월</option>
           <option value="6">최근 6개월</option>
           <option value="12">최근 1년</option>
@@ -61,59 +118,50 @@ function Table({ type }: TypeProps) {
         <thead className="border-t-2 border-b-2 border-primary py-20">
           <tr>
             <th className="py-2.5">번호</th>
-            <th>결제 날짜</th>
-            <th>결제 금액</th>
-            <th>충전 포인트</th>
+            <th>{type === "plus" ? "결제 날짜" : "사용 날짜"}</th>
+            <th>{type === "plus" ? "결제 금액" : "사용 내역"}</th>
+            <th>{type === "plus" ? "충전 포인트" : "사용 포인트"}</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
-          <tr>
-            <td className="py-2.5">1</td>
-            <td>2024.07.16</td>
-            <td>10,000</td>
-            <td>1,000</td>
-          </tr>
+          {pointList ? (
+            pointList.map((point, index) => (
+              // eslint-disable-next-line
+              <tr key={index}>
+                <td className="py-2.5">{index + 1}</td>
+                <td>
+                  {point.transactionDate.split("T")[0].replaceAll("-", ".")}
+                </td>
+                <td>{"money" in point ? point.money : point.detail}</td>
+                <td>{point.point}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={4} className="py-2.5">
+                데이터가 없습니다.
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
       <div className="my-3.5 flex justify-center">
         <div className="flex justify-between items-center w-80 text-h6">
-          <img src={preIcon} alt="이전버튼" className="w-[1rem] h-[1rem]" />
-          <div className="text-primary-700">1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <img src={nextIcon} alt="다음버튼" className="w-[1rem] h-[1rem]" />
+          <img
+            src={preIcon}
+            alt="이전버튼"
+            className="w-[1rem] h-[1rem]"
+            onClick={handlePreviousPageGroup}
+            role="presentation"
+          />
+          {renderPageNumbers()}
+          <img
+            src={nextIcon}
+            alt="다음버튼"
+            className="w-[1rem] h-[1rem]"
+            onClick={handleNextPageGroup}
+            role="presentation"
+          />
         </div>
       </div>
     </>
