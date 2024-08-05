60e2dc57c3f3756f19f15d0516aa0045fbbd1b64
seoh7dev <seoh7dev@gmail.com>
2024-08-05 11:34:40 +0900

    ✨ Feat : 크리에이터 리스트 검색 기능 구현

--- frontend/src/components/CreatorInfo.tsx
+++ frontend/src/components/CreatorInfo.tsx
@@ -1,8 +1,22 @@
-function CreatorInfo() {
+type Creator = {
+  creatorId: number;
+  creatorName: string;
+  profileImg: string;
+};
+
+interface CreatorProps {
+  creator: Creator;
+}
+
+function CreatorInfo({ creator }: CreatorProps) {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-[150px] h-[150px] bg-gray-300 rounded-full mb-2.5" />
-      <div>이름</div>
+      <img
+        src={creator.profileImg}
+        alt="프로필이미지"
+        className="w-[150px] h-[150px] bg-gray-300 rounded-full mb-2.5"
+      />
+      <div>{creator.creatorName}</div>
     </div>
   );
 }
