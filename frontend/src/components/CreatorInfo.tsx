8e70cbf91dcd7a9f466adfcc1b5a8404170a540b
seoh7dev <seoh7dev@gmail.com>
2024-08-06 09:53:35 +0900

    ✨ Feat : 프로필 이미지 클릭시 각 크리에이터 페이지로 이동

--- frontend/src/components/CreatorInfo.tsx
+++ frontend/src/components/CreatorInfo.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 type Creator = {
   creatorId: number;
   creatorName: string;
@@ -9,12 +11,16 @@ interface CreatorProps {
 }
 
 function CreatorInfo({ creator }: CreatorProps) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center">
       <img
         src={creator.profileImg}
         alt="프로필이미지"
         className="w-[150px] h-[150px] bg-gray-300 rounded-full mb-2.5"
+        onClick={() => navigate(`/creator/${creator.creatorId}`)}
+        role="presentation"
       />
       <div>{creator.creatorName}</div>
     </div>
