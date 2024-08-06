b7bafaabdf13955a2e33bdfc2e2a2ef538e22446
kiminchae <alsco9707@gmail.com>
2024-08-06 11:03:03 +0900

    ✨ Feat : navbar 경로추가 + hover추가

--- frontend/src/components/Navbar/CreatorNavbar.tsx
+++ frontend/src/components/Navbar/CreatorNavbar.tsx
@@ -1,16 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import useAuthStore from "../../store/useAuthStore";
+import client from "../../client";
 import creatorLogo from "../../assets/icons/logo/creator-logo.png";
 
 function CreatorNavbar() {
+  const token = useAuthStore((state) => state.accessToken);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    if (token) {
+      const response = await client(token).get("/api/member/logout");
+
+      if (response.status === 200) {
+        useAuthStore.getState().setAccessToken(null);
+        localStorage.removeItem("accessToken");
+        document.cookie = "refresh_token=; path=/; max-age=0;";
+        navigate("/");
+        window.location.reload();
+      }
+    }
+  };
+
   return (
     <div className="bg-white flex w-screen justify-between items-center h-20 px-10 py-2 shadow-nav-shadow">
       <div>
-        <img src={creatorLogo} alt="Logo" className="w-[265.27px] h-[44px]" />
+        <img
+          src={creatorLogo}
+          alt="Logo"
+          className="w-[265.27px] h-[44px] hover:cursor-pointer"
+          onClick={() => navigate("/main")}
+          role="presentation"
+        />
       </div>
       <div className="flex items-center">
-        <div className="text-secondary text-large font-bold m-10">
+        <div
+          className="text-secondary m-10 hover:cursor-pointer"
+          onClick={() => navigate("/creator-only")}
+          role="presentation"
+        >
           마이페이지
         </div>
-        <div className="creator-btn-light-lg">로그아웃</div>
+        <button
+          type="button"
+          className="creator-btn-light-lg"
+          onClick={handleLogout}
+        >
+          로그아웃
+        </button>
       </div>
     </div>
   );
