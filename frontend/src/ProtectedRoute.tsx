fb5081c30a3e803187a788e294a73128e5db842f
kiminchae <alsco9707@gmail.com>
2024-08-06 16:57:26 +0900

    🐛 Fix : 로그인안했는데 들어와지는 오류 수정

--- frontend/src/ProtectedRoute.tsx
+++ frontend/src/ProtectedRoute.tsx
@@ -17,17 +17,21 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
 
   useEffect(() => {
     const fetchUser = async () => {
-      if (!accessToken) {
-        navigate("/"); // 로그인 안 하면 랜딩페이지
+      setIsLoading(false);
+      if (isLoading) {
         return;
       }
-
+      if (!accessToken) {
+        navigate("/"); // 로그인 안 했으면 랜딩 페이지로 리다이렉트
+        return;
+      }
       try {
         const response = await client(accessToken).get("api/member");
         if (response.data.creator !== requiredCreator) {
           navigate("/error?code=UNAUTHORIZED&message=접근 권한이 없습니다.");
         } else {
           setIsCreator(response.data.creator);
+          setIsLoading(false);
         }
       } catch (error) {
         if (axios.isAxiosError(error)) {
@@ -35,13 +39,12 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
             `/error?code=${error.response?.data.errorCode}&message=${encodeURIComponent(error.response?.data.errorMessage)}`,
           );
         }
-      } finally {
         setIsLoading(false);
       }
     };
 
     fetchUser();
-  }, [accessToken, requiredCreator, navigate, setIsCreator]);
+  }, [accessToken, requiredCreator, navigate, setIsCreator, isLoading]);
 
   if (isLoading) {
     return <div>Loading...</div>;
