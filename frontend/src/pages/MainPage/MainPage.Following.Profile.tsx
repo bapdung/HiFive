9de1ed1a807052999f754403cd5a7f982364ebb8
Wonwoo Cho <ww8637@naver.com>
2024-08-06 18:01:04 +0900

    🚑 Hotfix : Creator 팔로잉한 크리에이터 프로필 이미지 오류 수정

--- frontend/src/pages/MainPage/MainPage.Following.Profile.tsx
+++ frontend/src/pages/MainPage/MainPage.Following.Profile.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 interface FollowingProfileProps {
   creatorId: number;
   profileName: string;
-  profileImage: string;
+  profile: string;
 }
 
 const FollowingProfile: React.FC<FollowingProfileProps> = ({
   creatorId,
   profileName,
-  profileImage,
+  profile,
 }) => (
   <div className="flex flex-col items-center">
     <div className="rounded-full overflow-clip bg-primary-100 shadow-lg mb-2">
       <Link to={`/creator/${creatorId}`}>
-        <img src={profileImage} alt="Profile" className="w-[150px] h-[150px]" />
+        <img src={profile} alt="Profile" className="w-[150px] h-[150px]" />
       </Link>
     </div>
     <span className="text-h4 text-gray-900">{profileName}</span>
