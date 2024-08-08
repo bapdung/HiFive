b8881ab39910cf8d80a6779c951a41d7e965533b
seojiheun <wlgmsaudqls@naver.com>
2024-08-08 15:42:42 +0900

    :construction: OpenVidu Category 수정 중

--- backend/src/main/java/com/ssafy/hifive/domain/openvidu/controller/OpenviduController.java
+++ backend/src/main/java/com/ssafy/hifive/domain/openvidu/controller/OpenviduController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,6 +65,11 @@ public class OpenviduController {
 		ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
 		Connection connection = session.createConnection(properties);
 		return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+	}
+
+	@PostMapping("/sessions/{sessionId}")
+	public ReseponseEntity<Void> currentCategory(@PathVariable("sessionId") String sessionId, @PathVariable("sequence") Integer sequence){
+		openViduService
 	}
 
 
