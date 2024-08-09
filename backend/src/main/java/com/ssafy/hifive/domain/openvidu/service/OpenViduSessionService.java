a057b8e944772820baf8253473dcd75b590af3a6
seojiheun <wlgmsaudqls@naver.com>
2024-08-09 15:54:01 +0900

    :sparkles: OpenVidu OpenSession Redis 저장 및 처리

---
+++ backend/src/main/java/com/ssafy/hifive/domain/openvidu/service/OpenViduSessionService.java
@@ -0,0 +1,41 @@
+package com.ssafy.hifive.domain.openvidu.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.hifive.domain.fanmeeting.entity.Fanmeeting;
+import com.ssafy.hifive.domain.fanmeeting.repository.FanmeetingRepository;
+import com.ssafy.hifive.global.error.ErrorCode;
+import com.ssafy.hifive.global.error.type.DataNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OpenViduSessionService {
+	private final FanmeetingRepository fanmeetingRepository;
+	private final RedisTemplate<String, String> redisTemplateForString;
+	private final RedisTemplate<String, Integer> redisTemplateForInteger;
+
+	public void saveSession(String fanmeetingId, String sessionId){
+		Fanmeeting fanmeeting = fanmeetingRepository.findById(Long.valueOf(fanmeetingId))
+			.orElseThrow(() -> new DataNotFoundException(ErrorCode.FANMEETING_NOT_FOUND));
+
+		String sessionkey = "fanmeeting:" + fanmeetingId + ":session";
+		redisTemplateForString.opsForValue().set(sessionkey, sessionId, fanmeeting.getRunningTime() + 30, TimeUnit.MINUTES);
+
+		String sequenceKey = "fanmeeting:" + fanmeetingId + ":sequence";
+		redisTemplateForInteger.opsForValue().set(sequenceKey, 0,fanmeeting.getRunningTime() + 30, TimeUnit.MINUTES);
+	}
+
+	public boolean isSessionOpen(String fanmeetingId) {
+		return redisTemplateForString.hasKey("fanmeeting:" + fanmeetingId + ":session");
+	}
+
+	public void deleteSession(String fanmeetingId) {
+		redisTemplateForString.delete("fanmeeting:" + fanmeetingId + ":session");
+		redisTemplateForInteger.delete("fanmeeting:" + fanmeetingId + ":sequence");
+	}
+}
