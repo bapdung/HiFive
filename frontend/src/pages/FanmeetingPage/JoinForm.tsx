9db3c0da1fa01b9925c00b1c357cf8cac00c6da9
kiminchae <alsco9707@gmail.com>
2024-08-10 13:40:36 +0900

    ✨ Feat  : 팬으로 접속하기 버튼 추가 (개발용)

--- frontend/src/pages/FanmeetingPage/JoinForm.tsx
+++ frontend/src/pages/FanmeetingPage/JoinForm.tsx
@@ -4,7 +4,7 @@ interface JoinFormProps {
   myUserName: string;
   mySessionId: string;
   isCreator: boolean | undefined;
-  // setIsCreator: (isCreator: boolean) => void;
+  setIsCreator: (isCreator: boolean) => void;
   joinSession: (e: FormEvent<HTMLFormElement>) => void;
 }
 
@@ -12,27 +12,37 @@ const JoinForm: React.FC<JoinFormProps> = ({
   myUserName,
   mySessionId,
   isCreator,
-  // setIsCreator,
+  setIsCreator,
   joinSession,
-}) => (
-  <div id="join">
-    <div id="join-dialog" className="jumbotron vertical-center">
-      <h1 className="text-h4"> 팬미팅 시작 화면 </h1>
-      <form className="form-group" onSubmit={joinSession}>
-        <p>참가자: {myUserName}</p>
-        <p>세션: {mySessionId}</p>
-        {isCreator ? "당신은 크리에이터입니다" : "당신은 팬입니다"}
-        <div className="text-center">
-          <input
-            className="btn btn-lg btn-success"
-            name="commit"
-            type="submit"
-            value="JOIN"
-          />
-        </div>
-      </form>
+}) => {
+  const handleSetFan = () => {
+    setIsCreator(false);
+  };
+  return (
+    <div id="join">
+      <div id="join-dialog" className="jumbotron vertical-center">
+        <h1 className="text-h4"> 팬미팅 시작 화면 </h1>
+        <form className="form-group" onSubmit={joinSession}>
+          <p>참가자: {myUserName}</p>
+          <p>세션: {mySessionId}</p>
+          {isCreator ? "당신은 크리에이터입니다" : "당신은 팬입니다"}
+          <div className="text-center">
+            <input
+              className="btn btn-lg btn-success"
+              name="commit"
+              type="submit"
+              value="JOIN"
+            />
+          </div>
+          {isCreator && (
+            <button type="button" onClick={handleSetFan}>
+              팬으로 접속하기
+            </button>
+          )}
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default JoinForm;
