2d59829bd374913d4a8b1fae600ecfb05b823b78
Wonwoo Cho <ww8637@naver.com>
2024-08-12 06:39:39 +0900

    대기방 css 수정

--- frontend/src/pages/FanmeetingPage/WaitingPage.tsx
+++ frontend/src/pages/FanmeetingPage/WaitingPage.tsx
@@ -1,25 +1,17 @@
-import ReactPlayer from "react-player/youtube";
-import BackGround from "../../assets/Fanmeeting/waitingBackground.png";
+import WaitingVideo from "./WaitingVideo";
+import BackGround from "../../assets/Fanmeeting/waitline.jpg";
+import logo from "../../assets/Fanmeeting/logoroom.png";
 
-const WaitingPage: React.FC = () => {
-  const videoUrl = "https://youtu.be/R_RAWjqdgTs?si=rCyikrDHdm7FwDdE";
-  // const fanmeetingDate = Date.now();
+const WaitingPage = () => (
+  <div className="bg-meetingroom-600 flex flex-col items-center w-full h-[100vh]">
+    <img src={logo} alt="logo" className="w-[200px] mr-4 my-10" />
+    <p className="text-h3 text-white mb-4">잠시 후 팬미팅이 시작됩니다!</p>
+    <WaitingVideo />
 
-  return (
-    <div className="bg-white flex flex-col justify-center items-center w-full relative">
-      <img src={BackGround} alt="background-img" className="w-full" />
-      <div className="p-[1rem] absolute z-30 top-[10%] left-[50%] bg-white rounded-[20px]">
-        <div id="waiting-video-container" className="rounded-[20px]">
-          <ReactPlayer url={videoUrl} />
-        </div>
-      </div>
-      <div>
-        <p className="text-[48px] text-white absolute top-[28%] left-28 z-30 font-[jua]">
-          잠시 후 팬미팅이 시작됩니다!
-        </p>
-      </div>
+    <div className="absolute bottom-0 left-0 w-full">
+      <img src={BackGround} alt="background-img" className="w-full h-auto" />
     </div>
-  );
-};
+  </div>
+);
 
 export default WaitingPage;
