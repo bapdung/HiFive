097a5f184751f8d310654ce55608250f24d40c9e
Wonwoo Cho <ww8637@naver.com>
2024-08-12 08:58:40 +0900

    팬미팅 방 css 시작

--- frontend/src/pages/FanmeetingPage/WaitingPage.tsx
+++ frontend/src/pages/FanmeetingPage/WaitingPage.tsx
@@ -1,11 +1,13 @@
 import WaitingVideo from "./WaitingVideo";
 import BackGround from "../../assets/Fanmeeting/waitline.jpg";
-import logo from "../../assets/Fanmeeting/logoroom.png";
+import logo from "../../assets/Fanmeeting/logomeeting.png";
 
 const WaitingPage = () => (
   <div className="bg-meetingroom-600 flex flex-col items-center w-full h-[100vh]">
-    <img src={logo} alt="logo" className="w-[200px] mr-4 my-10" />
-    <p className="text-h3 text-white mb-4">잠시 후 팬미팅이 시작됩니다!</p>
+    <img src={logo} alt="logo" className="w-[200px] mr-4 my-6" />
+    <p className="text-h4 text-white mb-4 font-semibold">
+      잠시 후 팬미팅이 시작됩니다!
+    </p>
     <WaitingVideo />
 
     <div className="absolute bottom-0 left-0 w-full">
