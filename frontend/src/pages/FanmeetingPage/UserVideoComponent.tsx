62996eb935f2405257cb28a5c47915f4e94fcf37
kiminchae <alsco9707@gmail.com>
2024-08-11 17:38:51 +0900

    ✨ Feat  : 정답 계산 (맞았는지 틀렸는지)

--- frontend/src/pages/FanmeetingPage/UserVideoComponent.tsx
+++ frontend/src/pages/FanmeetingPage/UserVideoComponent.tsx
@@ -1,6 +1,14 @@
 import OpenViduVideoComponent from "./OvVideo";
 
+interface Quiz {
+  problem: string;
+  answer: boolean;
+  totalQuizCount: number;
+  detail: string;
+}
+
 interface UserVideoComponentProps {
+  currentQuiz: Quiz | null;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   streamManager: any; // 비디오 스트림을 관리하는 객체
   userAnswers: { [key: string]: boolean };
@@ -11,50 +19,35 @@ const UserVideoComponent: React.FC<UserVideoComponentProps> = ({
   streamManager,
   userAnswers,
   isQuizTime,
+  currentQuiz,
 }) => {
-  const getNicknameTag = (): string =>
-    // Gets the nickName of the user
-    JSON.parse(streamManager.stream.connection.data).clientData;
+  const userId = streamManager.stream.connection.connectionId;
+  const userName = JSON.parse(streamManager.stream.connection.data).clientData;
+
+  const getNicknameTag = () => userName || "Unknown";
+
+  const isWrong =
+    isQuizTime && currentQuiz && userAnswers[userId] !== currentQuiz.answer;
+
   return (
     <div>
-      {streamManager !== undefined ? (
+      {streamManager && (
         <div className="streamcomponent">
           <OpenViduVideoComponent
             streamManager={streamManager}
-            userName={
-              JSON.parse(streamManager.stream.connection.data).clientData
-            }
+            userName={userName}
           />
           <div>
-            {streamManager !== undefined ? (
-              <div className="streamcomponent">
-                <OpenViduVideoComponent
-                  streamManager={streamManager}
-                  userName={
-                    JSON.parse(streamManager.stream.connection.data).clientData
-                  }
-                />
-                <div>
-                  {isQuizTime &&
-                    userAnswers &&
-                    (userAnswers[
-                      streamManager.stream.connection.connectionId
-                    ] != null ? (
-                      <p>
-                        {getNicknameTag()} 님이 선택한 답 :{" "}
-                        {userAnswers[
-                          streamManager.stream.connection.connectionId
-                        ]
-                          ? "O"
-                          : "X"}
-                      </p>
-                    ) : null)}
-                </div>
-              </div>
-            ) : null}
+            {isQuizTime && userAnswers[userId] != null && (
+              <p>
+                {getNicknameTag()} 님이 선택한 답 :{" "}
+                {userAnswers[userId] ? "O" : "X"}
+              </p>
+            )}
+            {isWrong && <p>틀렸어</p>}
           </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
