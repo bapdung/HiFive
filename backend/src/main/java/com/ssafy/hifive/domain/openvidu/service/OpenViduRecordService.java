8b53d08e4bb34303b4f36951f4a1833df611b2b5
김혁진 <gurwls0122@naver.com>
2024-08-12 17:32:37 +0900

    :ambulance: Hotfix : File Download 로직 수정

--- backend/src/main/java/com/ssafy/hifive/domain/openvidu/service/OpenViduRecordService.java
+++ backend/src/main/java/com/ssafy/hifive/domain/openvidu/service/OpenViduRecordService.java
@@ -1,10 +1,16 @@
 package com.ssafy.hifive.domain.openvidu.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.hifive.domain.file.service.FileService;
 import com.ssafy.hifive.domain.openvidu.dto.response.OpenViduRecordDto;
 import com.ssafy.hifive.global.error.ErrorCode;
 import com.ssafy.hifive.global.error.type.BadRequestException;
@@ -24,7 +30,8 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class OpenViduRecordService {
-	private final FileService fileService;
+	@Value("${openvidu.secret}")
+	private String openviduSecret;
 
 	public OpenViduRecordDto recordVideo(OpenVidu openVidu, String fanmeetingId) throws
 		OpenViduJavaClientException, OpenViduHttpException {
@@ -51,9 +58,42 @@ public class OpenViduRecordService {
 		openVidu.stopRecording(recordId);
 
 		log.info(openVidu.getRecording(recordId).getUrl());
-		fileService.downloadFile(openVidu.getRecording(recordId).getUrl(), "test");
+		downloadFile(openVidu.getRecording(recordId).getUrl(), "test");
 	}
 
-	private void unzipFileAndSaveS3(OpenVidu openVidu, String fanmeetingId, String url) throws IOException {
+	public void downloadFile(String fileUrl, String fileName) throws IOException {
+		URL url = new URL(fileUrl);
+		File destinationFile = new File("/app/recordings", fileName);
+
+		// Create URL connection
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		try {
+			// Set HTTP method
+			connection.setRequestMethod("GET");
+
+			// Set authorization header
+			String auth = "OPENVIDUAPP" + ":" + openviduSecret;
+			String encodeAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+			String authHeader = "Basic " + encodeAuth;
+			connection.setRequestProperty("Authorization", authHeader);
+
+			// Check response code
+			int responseCode = connection.getResponseCode();
+			if (responseCode != HttpURLConnection.HTTP_OK) {
+				throw new IOException("HTTP error code: " + responseCode);
+			}
+
+			// Read the input stream
+			try (InputStream inputStream = connection.getInputStream();
+				 FileOutputStream out = new FileOutputStream(destinationFile)) {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = inputStream.read(buffer)) != -1) {
+					out.write(buffer, 0, bytesRead);
+				}
+			}
+		} finally {
+			connection.disconnect();
+		}
 	}
 }
