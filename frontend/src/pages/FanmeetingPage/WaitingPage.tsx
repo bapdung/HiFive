c0a3e650dd77b2cf1c241a0dca1a4f0a2ff034f4
kiminchae <alsco9707@gmail.com>
2024-08-12 10:10:38 +0900

    ✨ Feat : 대기영상 링크 추가

--- frontend/src/pages/FanmeetingPage/WaitingPage.tsx
+++ frontend/src/pages/FanmeetingPage/WaitingPage.tsx
@@ -2,11 +2,15 @@ import WaitingVideo from "./WaitingVideo";
 import BackGround from "../../assets/Fanmeeting/waitline.jpg";
 import logo from "../../assets/Fanmeeting/logoroom.png";
 
-const WaitingPage = () => (
+interface WaitingProps {
+  waitingUrl: string | null;
+}
+
+const WaitingPage: React.FC<WaitingProps> = ({ waitingUrl }) => (
   <div className="bg-meetingroom-600 flex flex-col items-center w-full h-[100vh]">
     <img src={logo} alt="logo" className="w-[200px] mr-4 my-10" />
     <p className="text-h3 text-white mb-4">잠시 후 팬미팅이 시작됩니다!</p>
-    <WaitingVideo />
+    <WaitingVideo waitingUrl={waitingUrl} />
 
     <div className="absolute bottom-0 left-0 w-full">
       <img src={BackGround} alt="background-img" className="w-full h-auto" />
