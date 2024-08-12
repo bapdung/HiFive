70b3e1a9194f406649bf63f9959fb217742dc329
rkdalstj4505 <rkdalstj4505@naver.com>
2024-08-12 13:04:05 +0900

    🐛 Fix : 팬미팅 시간 표시

--- frontend/src/pages/FanmeetingPage/Main.tsx
+++ frontend/src/pages/FanmeetingPage/Main.tsx
@@ -18,7 +18,7 @@ import TimeTableComponent from "./TimeTableComponent";
 
 // "https://i11a107.p.ssafy.io/"
 const APPLICATION_SERVER_URL =
-  process.env.NODE_ENV === "production" ? "" : "http:localhost:8080/";
+  process.env.NODE_ENV === "production" ? "" : "https://i11a107.p.ssafy.io/";
 
 interface Timetable {
   categoryName: string;
