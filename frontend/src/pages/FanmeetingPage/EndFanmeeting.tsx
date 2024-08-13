81977eeef04673a206b20ea18da4b2b390ede177
kiminchae <alsco9707@gmail.com>
2024-08-14 08:00:23 +0900

    ✨ Feat : 추억저장소로 이동하는거 만들기

--- frontend/src/pages/FanmeetingPage/EndFanmeeting.tsx
+++ frontend/src/pages/FanmeetingPage/EndFanmeeting.tsx
@@ -5,9 +5,9 @@ const EndFanmeeting: React.FC = () => {
   const message = "이미 HiFive 한 팬미팅이에요.";
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center w-full min-h-[800px] bg-[#666666]">
+    <div className="flex flex-col items-center w-full min-h-[1200px] bg-[#666666]">
       <div className="w-1/3 flex flex-col items-center">
-        <img src={hifiveBird} alt="end-img" className="w-4/5 mb-6 my-40" />
+        <img src={hifiveBird} alt="end-img" className="w-4/5 mb-6 my-60" />
         <p className="text-h4 text-white my-3">{message}</p>
         <div className="w-full flex justify-center">
           <button
@@ -20,9 +20,9 @@ const EndFanmeeting: React.FC = () => {
           <button
             type="button"
             className="creator-btn-light-md w-1/3"
-            onClick={() => navigate("/creator/list")}
+            onClick={() => navigate("/mypage/gallery")}
           >
-            크리에이터 목록 보기
+            팬미팅 추억하러 가기
           </button>
         </div>
       </div>
