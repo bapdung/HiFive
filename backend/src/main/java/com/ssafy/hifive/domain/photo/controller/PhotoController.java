418f00b651df4478b0f388d209a58b0f30d7a93d
김혁진 <gurwls0122@naver.com>
2024-08-14 10:33:11 +0900

    ✨ Feat : Photo로직 생성

--- backend/src/main/java/com/ssafy/hifive/domain/photo/controller/PhotoController.java
+++ backend/src/main/java/com/ssafy/hifive/domain/photo/controller/PhotoController.java
@@ -32,14 +32,15 @@ public class PhotoController {
 
 	private final PhotoService photoService;
 
-	@Operation(summary = "사진 조회", description = "사용자가 참석한 전체 팬미팅에서 찍은 사진을 조회한다.")
+	@Operation(summary = "영상 전체 조회", description = "사용자가 참석한 전체 팬미팅에서 찍은 영상을 조회한다.")
 	@ApiResponse(responseCode = "401", description = "사용자 인증이 올바르지 않음",
 		content = @Content(mediaType = "application/json",
 			schema = @Schema(implementation = ErrorResponse.class),
 			examples = @ExampleObject(value = "{\"error\" : \"사용자 인증에 실패하였습니다.\"}")))
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<PhotoOverViewDto>> getPhotosByMember(@AuthenticationPrincipal Member member, @ModelAttribute PhotoParam param) {
-		return ResponseEntity.ok(photoService.getPhotosByMember(member, param));
+	public ResponseEntity<List<PhotoOverViewDto>> getPhotos(@AuthenticationPrincipal Member member,
+		@ModelAttribute PhotoParam param) {
+		return ResponseEntity.ok(photoService.getPhotos(member, param));
 	}
 
 }
