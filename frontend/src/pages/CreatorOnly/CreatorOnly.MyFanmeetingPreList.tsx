a02afe937f7315e522fad716c26edb9310ad28f7
Wonwoo Cho <ww8637@naver.com>
2024-08-15 18:40:02 +0900

    크리에이터 온리 정렬문제 수정

--- frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingPreList.tsx
+++ frontend/src/pages/CreatorOnly/CreatorOnly.MyFanmeetingPreList.tsx
@@ -49,22 +49,23 @@ const MyFanmeetingPreList: React.FC<MyFanmeetingPreListProps> = ({
 
   return (
     <div className="w-full overflow-x-auto py-4">
-      <div className="flex flex-nowrap space-x-4">
+      <div className="flex flex-wrap space-x-4 justify-between">
         {prevFanmeetings.length === 0 ? (
           <p className="text-xl text-gray-500 mr-auto ml-auto my-5">
             생성한 팬미팅이 없습니다.
           </p>
         ) : null}
         {prevFanmeetings.map((fanmeeting) => (
-          <MyFanmeetingItem
-            key={fanmeeting.fanmeetingId}
-            title={fanmeeting.title}
-            fanmeetingId={fanmeeting.fanmeetingId}
-            posterImg={fanmeeting.posterImg}
-            startDate={formatDate(fanmeeting.startDate)}
-            realStartDate={fanmeeting.startDate}
-            isDone={false}
-          />
+          <div key={fanmeeting.fanmeetingId} className="w-[19%] flex-shrink-0">
+            <MyFanmeetingItem
+              title={fanmeeting.title}
+              fanmeetingId={fanmeeting.fanmeetingId}
+              posterImg={fanmeeting.posterImg}
+              startDate={formatDate(fanmeeting.startDate)}
+              realStartDate={fanmeeting.startDate}
+              isDone={false}
+            />
+          </div>
         ))}
       </div>
     </div>
